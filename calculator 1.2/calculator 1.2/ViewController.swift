0d0fd113eeadcf3cb2664f6bd4aedffe155f87f8
Lounge Lizard dev/ <86478645+Lounge-Lizard-dev@users.noreply.github.com>
2021-11-28 12:45:33 +0100

    Finish the app
    You can use this app with a virtual machine on you mac. i ue browserstack

---
+++ calculator 1.2/calculator 1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator 1.2
+//
+//  Created by Lounge Lizard dev on 2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
